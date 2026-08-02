e7079025672a8be0d33cdbc835c538278ed8d503
Jose Luis Pitarch Miravete <jpitarch@mercadona.es>
2026-08-02 16:30:06 +0200

    fix: minor fix

--- src/main/java/dev/jpitarch/ctrlgym/core/repositories/jpa/MemberJpaRepository.java
+++ src/main/java/dev/jpitarch/ctrlgym/core/repositories/jpa/MemberJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -41,6 +42,7 @@ public interface MemberJpaRepository extends JpaRepository<UserMO, UserMO.ID> {
   Optional<String> getStripeSetupIntentId(String stripeCustomerId);
 
   @Modifying
+  @Transactional
   @Query("UPDATE UserMO u SET u.stripeSetupIntentId = :setupIntentId WHERE u.id = :memberId AND u.gymId = :gymId")
   void saveStripeSetupIntentId(UUID memberId, Integer gymId, String setupIntentId);
 }
